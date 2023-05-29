46a5931f9b140cdc2e6fabf32b66ab8f25d5ce8a
lsh4m4n <jsierra@lakeheadu.ca>
2023-05-28 21:04:17 -0500

    complete endpoints for all models
    issue number: UD006

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from structure.models import Org
+from structure.models import *
 
+
+# ===================
+# ==== Org related ==
+# ===================
 class OrgSerializerRestricted(serializers.ModelSerializer):
     class Meta:
         fields = ("name", "description")
@@ -13,7 +17,99 @@ class OrgSerializer(serializers.ModelSerializer):
         fields = ("name", "description", "password")
         model = Org
 
+class OrgSerializerFullRestricted(serializers.ModelSerializer):
+    class Meta:
+        fields = ()
+        model = Org
 
+
+# ====================
+# ==== Crop related ==
+# ====================
+class CropSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ("org", "name", "coordinate_latitude", "coordinate_longitude")
+        model = Crop
+
+class CropSerializerFullRestricted(serializers.ModelSerializer):
+    class Meta:
+        fields = ()
+        model = Crop
+
+
+# =============================
+# ==== Actuator_type related ==
+# =============================
+class Actuator_typeSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ( "name", "description",)
+        model = Actuator_type
+
+class Actuator_typeSerializerFullRestricted(serializers.ModelSerializer):
+    class Meta:
+        fields = ()
+        model = Actuator_type
+
+
+# ========================
+# ==== Actuator related ==
+# ========================
+class ActuatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ( "datetime", "crop", "value", "variable",)
+        model = Actuator
+
+class ActuatorSerializerFullRestricted(serializers.ModelSerializer):
+    class Meta:
+        fields = ()
+        model = Actuator
+
+
+# =========================
+# ==== Condition related ==
+# =========================
+class ConditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ( "crop", "variable", "min_value", "max_value",)
+        model = Condition
+
+class ConditionSerializerFullRestricted(serializers.ModelSerializer):
+    class Meta:
+        fields = ()
+        model = Condition
+
+
+# ===========================
+# ==== Measurement related ==
+# ===========================
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ( "datetime",  "crop", "value", "variable",)
+        model = Measurement
+
+class MeasurementSerializerFullRestricted(serializers.ModelSerializer):
+    class Meta:
+        fields = ()
+        model = Measurement
+
+
+# ========================
+# ==== Variable related ==
+# ========================
+class VariableSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ( "name", "units", "description",)
+        model = Variable
+
+class VariableSerializerFullRestricted(serializers.ModelSerializer):
+    class Meta:
+        fields = ()
+        model = Variable
+
+
+# ====================
+# ==== User related ==
+# ====================
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,51 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import *
 
 urlpatterns = [
-    path("orgs/<int:pk>/", OrgAPIDetail.as_view(), name="orgs_detail"),
-    path("orgs/new/", OrgAPICreate.as_view(), name="create_org"),
-    path('user/new/', CreateUserView.as_view(), name='create_user'),
-    path('user/login/', UserLoginView.as_view(), name='user_login'),
-    path('user/token_auth/', obtain_auth_token, name='api_token_auth'),
+    # ==== Org related
+    path("org/new/",          OrgAPICreate.as_view(),       name="create_org"),        # POST
+    path("org/<str:pk>/",     OrgAPIDetail.as_view(),       name="orgs_detail"),       # GET
+    path("org/mod/<str:pk>/", OrgAPIDetailModify.as_view(), name="orgs_detail"),       # PUT
+    path("org/del/<str:pk>/", OrgAPIDetailDelete.as_view(), name="orgs_detail"),       # DELETE
+
+    # ==== Crop related
+    path("crop/new/",          CropAPICreate.as_view(),       name="crop_create"),     # POST
+    path("crop/<str:pk>/",     CropAPIDetail.as_view(),       name="crop_detail"),     # GET
+    path("crop/mod/<str:pk>/", CropAPIDetailModify.as_view(), name="crop_modify"),     # PUT
+    path("crop/del/<str:pk>/", CropAPIDetailDelete.as_view(), name="crop_delete"),     # DELETE
+
+    # ==== Actuator related
+    path("actuator/new/",          ActuatorAPICreate.as_view(),       name="actuator_create"),     # POST
+    path("actuator/<str:pk>/",     ActuatorAPIDetail.as_view(),       name="actuator_detail"),     # GET
+    path("actuator/mod/<str:pk>/", ActuatorAPIDetailModify.as_view(), name="actuator_modify"),     # PUT
+    path("actuator/del/<str:pk>/", ActuatorAPIDetailDelete.as_view(), name="actuator_delete"),     # DELETE
+
+    # ==== Actuator_type related
+    path("actuatortype/new/",          Actuator_typeAPICreate.as_view(),       name="actuatortype_create"),     # POST
+    path("actuatortype/<str:pk>/",     Actuator_typeAPIDetail.as_view(),       name="actuatortype_detail"),     # GET
+    path("actuatortype/mod/<str:pk>/", Actuator_typeAPIDetailModify.as_view(), name="actuatortype_modify"),     # PUT
+    path("actuatortype/del/<str:pk>/", Actuator_typeAPIDetailDelete.as_view(), name="actuatortype_delete"),     # DELETE
+
+    # ==== Condition related
+    path("condition/new/",          ConditionAPICreate.as_view(),       name="condition_create"),     # POST
+    path("condition/<str:pk>/",     ConditionAPIDetail.as_view(),       name="condition_detail"),     # GET
+    path("condition/mod/<str:pk>/", ConditionAPIDetailModify.as_view(), name="condition_modify"),     # PUT
+    path("condition/del/<str:pk>/", ConditionAPIDetailDelete.as_view(), name="condition_delete"),     # DELETE
+
+    # ==== Measurement related
+    path("measurement/new/",          MeasurementAPICreate.as_view(),       name="measurement_create"),     # POST
+    path("measurement/<str:pk>/",     MeasurementAPIDetail.as_view(),       name="measurement_detail"),     # GET
+    path("measurement/mod/<str:pk>/", MeasurementAPIDetailModify.as_view(), name="measurement_modify"),     # PUT
+    path("measurement/del/<str:pk>/", MeasurementAPIDetailDelete.as_view(), name="measurement_delete"),     # DELETE
+
+    # ==== Variable related
+    path("variable/new/",          VariableAPICreate.as_view(),       name="variable_create"),     # POST
+    path("variable/<str:pk>/",     VariableAPIDetail.as_view(),       name="variable_detail"),     # GET
+    path("variable/mod/<str:pk>/", VariableAPIDetailModify.as_view(), name="variable_modify"),     # PUT
+    path("variable/del/<str:pk>/", VariableAPIDetailDelete.as_view(), name="variable_delete"),     # DELETE
+
+    # ==== User related
+    path('user/new/',        CreateUserView.as_view(), name='user_create'),
+    path('user/login/',      UserLoginView.as_view(),  name='user_login'),
+    path('user/token_auth/', obtain_auth_token,        name='user_api_token_auth'),
 ]
+

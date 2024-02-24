f729228d6c803e4a990b4dded16a48bfc8c88725
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2024-02-23 19:39:03 -0500

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -141,5 +141,6 @@ class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
+# coment
 
 
